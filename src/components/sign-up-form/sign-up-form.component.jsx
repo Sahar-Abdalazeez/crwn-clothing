67e5cb5b2b5f4179765ebbf85c3c5c38b7fb0b5b
sahar samer <sahar@aui.io>
2022-07-20 22:10:45 +0300

    build the sign up logic

---
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -0,0 +1,95 @@
+import { useState } from "react";
+import {
+  creatAuthUserWithEmailAndPassword,
+  createUserDocumentFromAuth,
+} from "../../utils/firebase/firebase.utils";
+
+
+  //initial field value =>empty fields
+  const defaultFormFields = {
+    displayName: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  };
+
+export const SignUpForm = () => {
+  const [formFields, setFormFields] = useState(defaultFormFields);
+  const { displayName, email, password, confirmPassword } = formFields;
+
+  //function to redet form fields when we added the user
+  const resetFormFields = () => {
+    setFormFields(defaultFormFields);
+  };
+
+//on change the value of any field 
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormFields({ ...formFields, [name]: value });
+  };
+
+  //function when submitting the form
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    if (password !== confirmPassword) {
+      alert("passwords do not match ");
+      return;
+    }
+
+    try {
+      const { user } = await creatAuthUserWithEmailAndPassword(email, password);
+      await createUserDocumentFromAuth(user, { displayName });
+      resetFormFields();
+    } catch (error) {
+      if (error.code === "auth/email-already-in-use") {
+        alert(" Cannot create user ,Email alredy used  ");
+      } else {
+        console.log(error);
+      }
+    }
+  };
+
+  return (
+    <div>
+      <h1>Signup with your email and password </h1>
+
+      <form onSubmit={handleSubmit}>
+        <label>Display Name </label>
+        {/* onchange the value we need to know which field is changing so we added the name  */}
+        <input
+          type="text"
+          required
+          name="displayName"
+          onChange={handleChange}
+          value={displayName}
+        />
+        <label> </label>Email
+        <input
+          type="email"
+          required
+          name="email"
+          onChange={handleChange}
+          value={email}
+        />
+        <label>Password </label>
+        <input
+          type="password"
+          required
+          name="password"
+          onChange={handleChange}
+          value={password}
+        />
+        <label>Confirm Password </label>
+        <input
+          type="password"
+          required
+          name="confirmPassword"
+          onChange={handleChange}
+          value={confirmPassword}
+        />
+        <button typr="submit">Sign up </button>
+      </form>
+    </div>
+  );
+};
--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,47 +1,22 @@
-import { useEffect } from "react";
-//method that
-import { getRedirectResult } from "firebase/auth";
 import {
-  auth,
   signInWithGooglePopup,
   createUserDocumentFromAuth,
-  signInWithGoogleRedirect,
 } from "../../utils/firebase/firebase.utils";
-import React from "react";
+import {SignUpForm} from '../../components/sign-up-form/sign-up-form.component';
 
 const SignIn = () => {
   // whenever we request data from data base we use async and await
   const logGoogleUser = async () => {
     const { user } = await signInWithGooglePopup();
-    //the bottom line just point to some place in the database
     createUserDocumentFromAuth(user);
   };
-  //this useEffect is needed when i want to use signInWithGoogleRedirect for now i'll go with signInWithGooglePopup
-  // useEffect(async()=>{
-  // const response = await getRedirectResult(auth);
-  // if(response)
-  // {
-  //   const userDocRef = await createUserDocumentFromAuth(response.user);
-  // console.log('userDocRef', userDocRef);
-  // }
-  // console.log('redirect response ', response);
-  // },[]);
 
-  //this code is not needed since if i want to use signInWithGoogleRedirect i could use it directly and get it's result
-  //by using getRedirectResult (auth)
-  // const logGoogleRedirectUser = async () => {
-  //   const { user } = await signInWithGoogleRedirect();
-  //   //any code after the sigining in with redirect will not be executed
-  //  console.log('user redirect', {user});
-  // };
 
   return (
     <div>
       sign in page
       <button onClick={logGoogleUser}>sign in with google popup </button>
-      {/* the commented code below is not needed since i'll go with signInWithGooglePopup */}
-      {/* <button onClick={logGoogleRedirectUser}>sign in with google redirect </button> */}
-      {/* <button onClick={signInWithGoogleRedirect}>sign in with google redirect </button> */}
+      <SignUpForm />
     </div>
   );
 };
