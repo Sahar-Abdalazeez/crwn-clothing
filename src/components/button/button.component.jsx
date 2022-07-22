12ed34f668aeb8e075e0b001671f7a7463805dc2
sahar samer <sahar@aui.io>
2022-07-22 19:55:11 +0300

    remove unneeded console

--- src/components/button/button.component.jsx
+++ src/components/button/button.component.jsx
@@ -2,7 +2,6 @@ import './button.styles.scss';
 import {BUTTON_TYPE_CLASSES} from '../../constants/button.constants';
 
   const Button = ({ children, buttonType, ...otherProps }) => {
-console.log('props',buttonType )
     return (
       <button
         {...otherProps}
