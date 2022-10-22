5ffa51d6aa9bf75c8a770d461ee370894d9ff7b1
Sahar samer <saharabdalazeez97@gmail.com>
2022-10-22 21:25:12 +0300

    convert button component to styled component

---
+++ src/components/button/button.styles.js
@@ -0,0 +1,99 @@
+import styled from "styled-components";
+
+export const BaseButton = styled.button`
+  min-width: 200px;
+  min-height: 50px;
+  max-height: 70px;
+  letter-spacing: 0.5px;
+  line-height: 0.5px;
+  padding: 0 15px 0 15px;
+  font-size: 15px;
+  background-color: black;
+  color: white;
+  text-transform: uppercase;
+  font-weight: bolder;
+  border: none;
+  cursor: pointer;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+
+  &:hover {
+    background-color: white;
+    color: black;
+    border: 1px solid black;
+  }
+`;
+
+export const GoogleButton = styled(BaseButton)`
+  background-color: #4285f4;
+  color: white;
+
+  &:hover {
+    background-color: #357ae8;
+    border: none;
+  }
+`;
+
+export const InvertedButton = styled(BaseButton)`
+  background-color: white;
+  color: black;
+  border: 1px solid black;
+
+  &:hover {
+    background-color: black;
+    color: white;
+    border: none;
+  }
+`;
+
+// .button-container {
+//   // min-width: 165px;
+//   min-width: 200px;
+//   // width:150px;
+//   min-height: 50px;
+//   max-height: 70px;
+//   letter-spacing: 0.5px;
+//   line-height: 0.5px;
+//   padding: 0 15px 0 15px;
+//   font-size: 15px;
+//   background-color: black;
+//   color: white;
+//   text-transform: uppercase;
+//   font-weight: bolder;
+//   border: none;
+//   cursor: pointer;
+//   display: flex;
+//   justify-content: center;
+//   align-items: center;
+//   text-align: center;
+
+//   &:hover {
+//     background-color: white;
+//     color: black;
+//     border: 1px solid black;
+//   }
+
+//   &.google-sign-in {
+//     background-color: #4285f4;
+//     color: white;
+
+//     &:hover {
+//       background-color: #357ae8;
+//       border: none;
+//     }
+//   }
+
+//   &.inverted {
+//     background-color: white;
+//     color: black;
+//     border: 1px solid black;
+
+//     &:hover {
+//       background-color: black;
+//       color: white;
+//       border: none;
+//     }
+//   }
+// }
--- src/constants/button.constants.js
+++ src/constants/button.constants.js
@@ -1,4 +1,5 @@
 export const BUTTON_TYPE_CLASSES = {
+  BASE: "base",
   GOOGLE: "google-sign-in",
   INVERTED: "inverted",
 };
