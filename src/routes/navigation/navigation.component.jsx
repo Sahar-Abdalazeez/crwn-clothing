333143818d1256dbe520aa8dc320a14eddc0884f
“sahar <sahar@aui.io>
2023-11-22 21:42:33 +0200

    user selector

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -6,11 +6,12 @@ import { signOutUser } from "../../utils/firebase/firebase.utils";
 import CartIcon from "../../components/cart-icon/cart-icon.component";
 import CartDropdown from "../../components/cart-dropdown/cart-dropdown.component";
 import { CartContext } from "../../contexts/cart.context";
+import { useSelector } from "react-redux";
 import { NavigationContainer, LogoContainer, NavLinks, NavLink } from './navigation.styles';
+import {selectCurrentUser} from '../../store/user/user.selector'
 const Navigation = () => {
-  const { currentUser } = useContext(UserContext);
   const { isCartOpened } = useContext(CartContext);
-
+  const currentUser = useSelector(selectCurrentUser);
   return (
     <Fragment>
 
