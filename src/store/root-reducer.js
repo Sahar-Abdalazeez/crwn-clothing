893684c79a8dc428376dbafed793f17076479618
“sahar <sahar@aui.io>
2023-11-24 22:00:26 +0200

    user reducer

--- src/store/root-reducer.js
+++ src/store/root-reducer.js
@@ -1,7 +1,9 @@
 //combineReducers method that allow us to create big reducer from smaller ones 
 import {combineReducers} from 'redux';
-import {userReducer} from './user/user.reducer'
+import {userReducer} from './user/user.reducer';
+import {categoriesReducer} from './categories/categories.reducer';
 //we are going to pass object with the keys and the values the name of the reduce slice then the actual reducer itself 
 export const rootReducer =combineReducers({
-    user: userReducer
+    user: userReducer, 
+    categories: categoriesReducer, 
 })
