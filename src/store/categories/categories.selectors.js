37859119a293504ca71e8c16b8e9a81f1567184f
“sahar <sahar@aui.io>
2023-11-28 08:12:08 +0200

    use reselect for memoization

--- src/store/categories/categories.selectors.js
+++ src/store/categories/categories.selectors.js
@@ -1,7 +1,34 @@
-export const selectCategoriesMap = (state) =>
-  state.categories.categories.reduce((acc, category) => {
-    //we changed the object as we want [{title:'',items:[]}]
-    const { title, items } = category;
-    acc[title.toLowerCase()] = items;
-    return acc;
-  }, {});
+import {createSelector} from 'reselect'; 
+
+
+const selectCategoryReducer = (state) => state.categories;
+
+
+const selectCategories = createSelector(
+  [selectCategoryReducer],
+  (categoriesSlice)=>categoriesSlice.categories
+)
+
+
+export const selectCategoriesMap = createSelector(
+  [selectCategories], 
+  (categories)=>{
+    return  categories.reduce((acc, category) => {
+      //we changed the object as we want [{title:'',items:[]}]
+      const { title, items } = category;
+      acc[title.toLowerCase()] = items;
+      return acc;
+    }, {});
+  }
+)
+
+//we used memization above instead of the following code 
+// export const selectCategoriesMap = (state) =>{
+// console.log('===== newwwwwselector of select categories  fired');
+//  return  state.categories.categories.reduce((acc, category) => {
+//     //we changed the object as we want [{title:'',items:[]}]
+//     const { title, items } = category;
+//     acc[title.toLowerCase()] = items;
+//     return acc;
+//   }, {});
+// }
